54d7951ff535129f8aaa3ccb21ed3fa0b1903092
Yüşa Furkan <71369719+yehpop@users.noreply.github.com>
2020-11-19 19:35:16 +0300

    DON'T SELECT BUCKET

--- lib/tools/Bucket.py
+++ lib/tools/Bucket.py
@@ -8,3 +8,17 @@ class ToolBucket:
             'palette_color': pygame.Color(paletteColor.r, paletteColor.g, paletteColor.b, 255),
             'opacity': opacity
         }  # yapf: enables
+
+
+    def set_option(self, optionID, value):
+        if optionID in self.option_data:
+            self.option_data[optionID] = value
+
+    def process_canvas(self, event, canvas, mousePos):
+        pass
+
+    def update(self, timeDelta, canvasSurface, canvasPos, canvas):
+        pass
+
+    def process_event(self, event):
+        pass
